9ba1627ed4fc3482f144591886ec29dda2860616
omid345 <omid.345@gmail.com>
2021-09-10 00:10:47 -0400

    updated projects section and reduced the delay animations

--- src/mock/data.js
+++ src/mock/data.js
@@ -19,11 +19,11 @@ export const heroData = {
 export const aboutData = {
   img: 'profile.jpg',
   paragraphOne:
-    'A little about me, I am a 2020 computer science graduate. I finished school during a pandemic and I have been working remotely since then.',
+    'A little about me, I am a 2020 computer science graduate. Since finishing school, I have been working remotely for the past year.',
   paragraphTwo:
-    "I like trying new languages, frameworks and any technology that I come across. I'm not afraid of failure, but never having tried!",
+    'I like trying new languages, frameworks and any technology that I come across.',
   paragraphThree:
-    "I'm pretty involved in my community, specifically the Afghan community. My people the hazara are at risk minority under the oppresive regime of Taliban.",
+    "In the future I'd like to travel to Afghanistan and teach programming using my native tongue Farsi/Dari.",
   resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
 };
 
@@ -31,30 +31,48 @@ export const aboutData = {
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'heroku-fullstack.png',
+    title: 'Activity suggestions',
+    info: 'A simple fullstack todo list that uses react and node to store and retrieve random tasks from an api call.',
+    info2: 'Tech: React, Nodejs, PostgreSQL, on Heroku,',
+    url: 'https://deploy-fullstack-heroku.herokuapp.com/',
+    repo: 'https://github.com/omid345/deploying-fullstack-with-heroku-sample.git', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'express-library.png',
+    title: 'Local Library',
+    info: 'A fullstack library site that uses pug, template web engine, to store and retrieve books.',
+    info2: 'Tech: Pug, Nodejs, mongoDB, on Heroku',
+    url: 'https://express-library-omid.herokuapp.com/catalog',
+    repo: 'https://github.com/omid345/express-locallibrary.git', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'codestrips.png',
+    title: 'Codestrips',
+    info: 'A simple fullstack comic strip site, where user can store a comic they created.',
+    info2: 'React, Nodejs, uses local sqlite',
+    url: 'https://mini-capstone-codestrips.herokuapp.com/',
+    repo: 'https://github.com/omid345/mini-capstone-codestrips', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'Jamming.png',
+    title: 'Jamming',
+    info: 'User can search for music then add selected music to a new playlist on their Spotify account.',
+    info2: 'Tech: React, Nodejs, on surge.sh',
+    url: 'http://listify.surge.sh/',
+    repo: 'https://github.com/omid345/listify', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'old-site.png',
+    title: 'Portfolio',
+    info: 'My first portfolio I coded without any fancy frameworks.',
+    info2: 'Tech: Bootstrap, HTML, on github pages',
+    url: 'https://omid345.github.io/final-sites/',
+    repo: 'https://github.com/omid345/final-sites/tree/gh-pages', // if no repo, the button will not show up
   },
 ];
 
